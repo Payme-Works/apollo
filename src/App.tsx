9ed57886e1bfc2596a9fc1011925430aabe9d643
André "Dezzy <andrevcv1@gmail.com>
2021-07-30 19:20:54 -0300

    chore: Remove ares utils

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import { schema } from '@/store/config';
 import { GlobalStyle, Window } from '@/styles/global';
 import darkTheme from '@/styles/themes/dark';
 import lightTheme from '@/styles/themes/light';
-import startAresPythonServer from '@/utils/ares/startAresPythonServer';
 
 import '../i18n';
 
@@ -34,9 +33,7 @@ const App = () => {
   });
 
   useEffect(() => {
-    startAresPythonServer().then(() => {
-      setLoading(false);
-    });
+    setLoading(false);
   }, []);
 
   return (
