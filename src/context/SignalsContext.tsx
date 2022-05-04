d35e481594503f6fac53b0b7add7b52d2b832f06
André "Dezzy" Victor <hi@andrevictor.me>
2022-05-03 23:43:19 -0300

    chore: Update signals

--- src/context/SignalsContext.tsx
+++ src/context/SignalsContext.tsx
@@ -122,36 +122,36 @@ export function SignalsContextProvider({ children }) {
         return joinSignals; */
 
         const signalsTemplate = `
-          M1;02:32;EURUSD;PUT️
-          M1;03:47;EURUSD;CALL
-          M1;04:57;GBPUSD;CALL
-          M1;08:47;EURUSD;CALL
-          M1;09:22;EURUSD;CALL
-          M1;10:12;EURJPY;CALL
-          M1;11:32;GBPJPY;PUT
-          M1;12:22;EURJPY;PUT
-          M1;13:12;EURGBP;PUT
-          M1;14:32;EURUSD;CALL
-          M5;00:25;EURUSD;PUT
-          M5;02:05;AUDUSD;PUT
-          M5;03:35;EURUSD;CALL
-          M5;03:55;AUDUSD;PUT️
-          M5;04:45;USDCAD;PUT
-          M5;04:50;GBPUSD;CALL
-          M5;06:20;AUDJPY;PUT
-          M5;07:45;EURGBP;PUT️
-          M5;08:00;EURUSD;PUT
-          M5;08:45;AUDJPY;CALL
-          M5;09:20;GBPJPY;CALL
-          M5;11:05;EURUSD;PUT
-          M5;11:20;EURUSD;PUT
-          M5;11:35;GBPJPY;PUT
-          M5;11:55;EURGBP;CALL
-          M5;12:20;EURJPY;PUT
-          M5;13:30;AUDJPY;PUT
-          M5;13:55;EURGBP;CALL
-          M5;14:15;EURJPY;CALL
-          M5;15:55;EURUSD;PUT️
+          M1;02:12;AUDUSD;PUT
+          M1;04:17;AUDJPY;CALL
+          M1;04:22;USDCAD;PUT
+          M1;05:47;GBPJPY;CALL
+          M1;07:17;AUDJPY;CALL
+          M1;07:37;GBPUSD;PUT️
+          M1;08:37;EURGBP;PUT️
+          M1;10:17;AUDJPY;CALL️
+          M1;10:22;AUDCAD;PUT
+          M1;13:42;GBPJPY;PUT
+          M5;01:05;EURUSD;PUT
+          M5;03:00;AUDUSD;PUT️
+          M5;03:10;USDJPY;CALL
+          M5;04:20;AUDJPY;CALL️
+          M5;05:00;EURJPY;CALL
+          M5;05:45;GBPJPY;CALL
+          M5;06:05;EURUSD;CALL
+          M5;07:15;AUDCAD;CALL
+          M5;07:30;GBPJPY;PUT
+          M5;07:35;GBPUSD;PUT
+          M5;07:40;AUDUSD;PUT️
+          M5;08:20;AUDJPY;PUT
+          M5;08:35;EURGBP;PUT️
+          M5;09:40;EURJPY;CALL
+          M5;10:15;GBPJPY;CALL
+          M5;11:35;AUDCAD;CALL
+          M5;13:25;EURGBP;PUT️
+          M5;13:40;GBPJPY;PUT
+          M5;14:05;EURGBP;PUT
+          M5;16:45;EURGBP;CALL️
         `;
 
         const parsedSignals = signalsTemplate
